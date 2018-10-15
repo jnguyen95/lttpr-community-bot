394d2cb889e51ee30ef82f626054ca7c9f5a3de4
Justin Nguyen <jnnguyen50@gmail.com>
2018-10-14 20:13:26 -0500

    adding a few test commands

--- index.js
+++ index.js
@@ -15,20 +15,32 @@ client.on("message", async message => {
 
     switch(command) {
         case "create":
+            var author = message.author;
+            message.channel.send(author.toString() + ', created new race #id!');
             break;
         case "races":
             break;
         case "raceinfo":
             break;
         case "join":
+            var author = message.author;
+            message.channel.send(author.toString() + ' has joined race #id!');
             break;
         case "leave":
+            var author = message.author;
+            message.channel.send(author.toString() + ' has left race #id.');
             break;
         case "remove":
             break;
         case "done":
+            var author = message.author;
+            // Debugging purposes!
+            var time = timeconverter.convertToTime(72063);
+            message.channel.send(author.toString() + ' has completed race #id with time: ' + )
             break;
         case "forfeit":
+            var author = message.author;
+            message.channel.send(author.toString() + ' has forfeited race #id.')
             break;
         case "result":
             break;

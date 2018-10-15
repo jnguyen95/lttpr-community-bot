930c41de00f01fcd459341a82e9d98e5fc08dc57
Justin Nguyen <jnnguyen50@gmail.com>
2018-10-14 20:41:00 -0500

    adding padding logic and fixing seconds typo

--- modules/timeconverter.js
+++ modules/timeconverter.js
@@ -2,11 +2,19 @@ const math = require('mathjs');
 
 exports.convertToTime = function(milliseconds) {
     var toSeconds = math.floor(milliseconds / 1000);
-    var seconds = milliseconds % 60;
+    var seconds = toSeconds % 60;
     toSeconds = toSeconds - seconds;
     var toMinutes = toSeconds / 60;
     var minutes = toMinutes % 60;
     toMinutes = toMinutes - minutes;
     var hours = toMinutes / 60;
-    return hours.toString() + ':' + minutes.toString() + ':' + seconds.toString();
+    return padTime(hours.toString()) + ':' + padTime(minutes.toString()) + ':' + padTime(seconds.toString());
+}
+
+// This padding will always be of length 2
+function padTime(time) {
+    if (time.length === 1) {
+        return '0' + time;
+    }
+    return time;
 }
